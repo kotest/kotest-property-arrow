dcaff79c3467398ff63d69f9f4b32f7698f58fde
sksamuel <sam@sksamuel.com>
2021-05-12 05:21:04 -0500

    Bumped arrow version

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -4,13 +4,13 @@ object Libs {
    const val org = "io.kotest.extensions"
 
    object Kotest {
-      private const val version = "4.5.0.RC1"
+      private const val version = "4.5.0"
       const val Property = "io.kotest:kotest-property:$version"
       const val junit5 = "io.kotest:kotest-runner-junit5-jvm:$version"
    }
 
    object Arrow {
-      private const val version = "0.13.1"
+      private const val version = "0.13.2"
       const val core = "io.arrow-kt:arrow-core:$version"
    }
 }
